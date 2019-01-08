b535b89be7e3330a45d7c7b61bacdbf080904f60
Forhan Noor <fnoor@Amatrol.com>
2019-01-08 12:10:57 -0500

    Database.java modified to parse CSV successfully, accessor-mutator for all classes implemented

--- src/Database.java
+++ src/Database.java
@@ -1,5 +1,6 @@
 import java.awt.Image;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -9,42 +10,45 @@ public class Database {
 	private String imagePath;
 	private Item [] items;
 	private Image defaultImage;
-	private final String DEFAULT_IMG = "pexels-photo-1549702.jpg";
-	private final int SIZE = 199; // inventory size
+	private final String DEFAULT_IMG = "pexels-photo-1549702.jpeg";
 	
 	public Database(String productDB, String imagePath) {
 		this.productDB = productDB;
 		this.imagePath = imagePath;
-		//defaultImage = getImage(DEFAULT_IMG);
+		defaultImage = getImage(DEFAULT_IMG);
 	}
 	
+	// parse CSV file to initialize list of items
 	public void loadInventory(){
 		try {
+			ArrayList <Item> lst = new ArrayList<Item>(50);
 			File file = new File(productDB);
 			Scanner s = new Scanner(file);
-			items = new Item [SIZE];
-			s.useDelimiter(",");
+			s.useDelimiter(",|\n");
 			s.nextLine();
 			String a, b, c, d;
 			int e;
 			double f, g, h;
 			
-			for(int i = 0; i < SIZE; i++){
-				a = s.next();
-				b = s.next();
-				c = s.next();
-				d = s.next();
-				e = Integer.parseInt(s.next());
-				f = Double.parseDouble(s.next());
-				g = Double.parseDouble(s.next());
-				h = Double.parseDouble(s.next());
-				items[i] = new Item(a, b, new Product(c, d, f), e, h);
+			while(s.hasNext()){
+				a = s.next(); // product line
+				b = s.next(); // product type
+				c = s.next(); // product title
+				d = s.next(); // product description
+				e = Integer.parseInt(s.next()); // stock 
+				f = Double.parseDouble(s.next()); // unit price
+				g = Double.parseDouble(s.next()); // retail price
+				h = Double.parseDouble(s.next()); // profit margin
+				lst.add(new Item(a, b, new Product(c, d, f), e, h));
 			}
+			
+			items = lst.toArray(new Item [0]);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 	
+	// return ImageIcon given a file name
 	public ImageIcon getIcon(String name){
 		ImageIcon icon = null;
 		
@@ -58,6 +62,7 @@ public class Database {
 		return icon;
 	}
 	
+	// return Image given a file name
 	public Image getImage(String name){
 		Image img = null;
 		
--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,5 @@
 public class Item {
+	private String sku;
     private String productLine;
 	private String productType;
 	private Product product;
@@ -6,15 +7,77 @@ public class Item {
 	private double retailPrice;
 	private double profitMargin;
 	
-	public Item(String productLine, String productType, Product product, int stock, double profitMargin){
+	private static StringBuilder sb = new StringBuilder();
+	
+	public Item(String productLine, String productType, Product product, int stock, double retailPrice, double profitMargin){
 		this.productLine = productLine;
 		this.productType = productType;
 		this.product = product;
 		this.stock = stock;
+		this.retailPrice = retailPrice;
 		this.profitMargin = profitMargin / 100;
-		double p = product.getUnitPrice();
-		this.retailPrice = p + p * profitMargin;
+		
+		// generate SKU
+		sb.setLength(0);
+		String [] tokens = productLine.split(" ");
+		
+		for(String t: tokens){
+			sb.append(t.charAt(0));
+		}
+		
+		sb.append("-");
+		tokens = productType.split(" ");
+		
+		for(String t: tokens){
+			sb.append(t.charAt(0));
+		}
+		
+		sb.append("-");
+		sb.append(product.getTitle().substring(0, 2));
+		tokens = product.getDescription().split(" ");
+		
+		for(String t: tokens){
+			sb.append("-");
+			
+			if(t.length() < 4){
+				sb.append(t);
+			}
+			
+			else{
+				sb.append(t.substring(0, 2));
+			}
+		}
+		
+		sku = sb.toString().toUpperCase();
 	}
 	
-	public String toString(){ return productLine + " " + productType + " " + product + " " + stock + " " + retailPrice + " " + profitMargin; }
+	public String getSku(){ return sku; }
+	
+	public void setSku(String p){ sku = p; }
+	
+	public String getProductLine(){ return productLine; }
+	
+	public void setProductLine(String p){ productLine = p; }
+	
+	public String getProductType(){ return productType; }
+	
+	public void setProductType(String p){ productType = p; }
+	
+	public Product getProduct(){ return product; }
+	
+	public void setProduct(Product p){ product = p; }
+	
+	public int getStock(){ return stock; }
+	
+	public void setStock(int s){ stock = s; }
+	
+	public double getRetailPrice(){ return retailPrice; }
+	
+	public void setRetailPrice(double p){ retailPrice = p; }
+	
+	public double getProfitMargin(){ return profitMargin; }
+	
+	public void setProfitMargin(double p){ profitMargin = p; }
+	
+	public String toString(){ return sku + " " + productLine + " " + productType + " " + product + " " + stock + " " + retailPrice + " " + profitMargin; }
 }
